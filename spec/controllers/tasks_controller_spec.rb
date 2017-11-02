c16899268c1852e72d20445f6d86fde273bd6938
Tomas Espina <tespintom@icloud.com>
2017-11-02 15:50:19 +0000

    Toggling Completion

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -6,9 +6,18 @@ RSpec.describe TasksController, type: :controller do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
       get :index
-      expect(response).to have_http_status :sucess
+      expect(response).to have_http_status :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
+    end
+  end
+  describe 'task#update' do
+    it 'should allow tasks to be marked as done' do
+      task = FactoryBot.create(:task, done: false)
+      put :update, id:task.id, task: { done: true }
+      expect(response).to have_http_status(:success)
+      task.reload
+      expect(task.done).to eq(true)
     end
   end
 end
