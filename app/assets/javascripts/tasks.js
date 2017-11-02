9955ea1346816803ad7e4d9f25676c1f2d8b8aa2
Tomas Espina <tespintom@icloud.com>
2017-11-02 19:17:01 +0000

    adding strikethrough

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -5,7 +5,9 @@ $(function() {
   // <li> tags
   function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
-    var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
     " data-id='" + task.id + "'" +
     checkedStatus +
     '><label>' +
@@ -27,6 +29,12 @@ $(function() {
       task: {
         done: doneValue
       }
+    }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $('#listItem-' + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
+
     });
   }
   $.get("/tasks").success( function( data ) {
@@ -57,6 +65,7 @@ $(function() {
         var ulTodos = $('.todo-list');
         ulTodos.append(htmlString);
         $('.toggle').click(toggleTask);
+        $('.new-todo').val('');
       });
     });
 
