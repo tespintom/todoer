97a9a21078f3a8841147e7c0aeca67c1aeb75439
Tomas Espina <tespintom@icloud.com>
2017-11-02 18:31:39 +0000

    add task on page

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  describe "tasks#index" do
-    it "should list the tasks in the database" do
+  describe 'tasks#index' do
+    it 'should list the tasks in the database' do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
-      task1.update_attributes(title: "Something else")
+      task1.update_attributes(title: 'Something else')
       get :index
       expect(response).to have_http_status :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
       response_ids = response_value.collect do |task|
-        task["id"]
+        task['id']
       end
       expect(response_ids).to eq([task1.id, task2.id])
     end
   end
-  describe "tasks#update" do
-    it "should allow tasks to be marked as done" do
+  describe 'tasks#update' do
+    it 'should allow tasks to be marked as done' do
       task = FactoryBot.create(:task, done: false)
       put :update, id: task.id, task: { done: true }
       expect(response).to have_http_status(:success)
@@ -26,4 +26,13 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
+  describe 'task#create' do
+    it 'should allow new task to be created' do
+      post :create, task: { title: 'Fix things' }
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq('Fix things')
+      expect(Task.last.title).to eq('Fix things')
+    end
+  end
 end
